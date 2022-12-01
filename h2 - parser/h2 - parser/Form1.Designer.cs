9c86966a3e6803a145cf3cda4b01f1b8e3fa2ff3
GianlucaG00 <gianlu.gambi@gmail.com>
2022-12-01 23:49:52 +0100

    parser

---
+++ h2 - parser/h2 - parser/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace h2___parser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Bahnschrift Condensed", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(529, 142);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(267, 58);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Choose CSV file";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(31, 246);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(1258, 642);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(540, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(256, 58);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "CSV PARSER";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1326, 927);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ h2 - parser/h2 - parser/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace h2___parser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            string instructions = "CSV parser: choose a file and the program will parse every line deleting the separator"; 
+            richTextBox1.AppendText(instructions + "\n");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            // we filter the files in order to open only csv files
+            ofd.Title = "Open a CSV file";
+            ofd.Filter = "CSV file|*.csv";
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string[] csvText = System.IO.File.ReadAllLines(ofd.FileName);
+                richTextBox1.Text = "SELECTED FILE: " + ofd.FileName + "\n-------------------------------------------------\n"; 
+
+                for (int i=0; i < csvText.Length; i++)
+                {
+                    string[] data = csvText[i].Split('\n');
+
+                    for(int j=0; j<data.Length; j++)
+                    {
+                        string[] token = data[j].Split(','); 
+
+                        for(int k=0; k<token.Length; k++)
+                        {
+                            richTextBox1.AppendText(token[k] + "\n");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
